ba4375029357830fb24876cca596c475dc864d34
Alexandre <alex.menin11@gmail.com>
2020-04-09 17:37:29 -0300

    feat: add files .SCSS and animate title

---
+++ js/main.js
@@ -0,0 +1,44 @@
+jQuery(function () {
+
+  new WOW().init();
+
+  titleAnimate = () => {
+    anime.timeline({
+        loop: false
+      })
+      .add({
+        targets: '.title .line',
+        opacity: [0.5, 1],
+        scaleX: [0, 1],
+        easing: "easeInOutExpo",
+        duration: 700
+      }).add({
+        targets: '.title .line',
+        duration: 600,
+        easing: "easeOutExpo",
+        translateY: (el, i) => (-0.625 + 0.625 * 2 * i) + "em"
+      }).add({
+        targets: '.title .letters-left',
+        opacity: [0, 1],
+        translateX: ["0.5em", 0],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=300'
+      }).add({
+        targets: '.title .letters-right',
+        opacity: [0, 1],
+        translateX: ["-0.5em", 0],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=600'
+      }).add({
+        targets: '.title',
+        opacity: 1,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+      });
+  }
+
+  titleAnimate();
+});
